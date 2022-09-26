53a0a741191bb62abd4a42954d3943a6a6ace10c
SseoheeKim <108666056+SseoheeKim@users.noreply.github.com>
2022-09-26 18:36:05 +0900

    게시판

--- Board/src/jdbc/main/run/MainRun.java
+++ Board/src/jdbc/main/run/MainRun.java
@@ -1,8 +1,10 @@
 package jdbc.main.run;
 
+import jdbc.main.view.MainView;
+
 public class MainRun {
 	public static void main(String[] args) {
-	
+		new MainView().mainMenu();
 		
 	}
 }
--- Board/src/jdbc/main/view/MainView.java
+++ Board/src/jdbc/main/view/MainView.java
@@ -1,5 +1,75 @@
 package jdbc.main.view;
 
-public class MainView {
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+import jdbc.main.model.service.MainService;
+import jdbc.main.model.vo.Member;
+
+public class MainView {
+	private Scanner sc = new Scanner(System.in);
+	private MainService service = new MainService();
+	
+	public static Member loginMember = null;
+	
+	public void mainMenu() {
+		int input = -1;
+		
+		
+		do {
+			
+			try {
+				
+				if(loginMember == null) {
+					System.out.println("\n***** 회원제 게시판 프로그램 *****\n");
+					System.out.println("1. 로그인");
+					System.out.println("2. 회원 가입");
+					System.out.println("0. 프로그램 종료");
+	
+					System.out.print("\n메뉴 선택 : ");
+	
+					input = sc.nextInt();
+					sc.nextLine(); // 입력 버퍼 개행문자 제거
+					System.out.println();
+	
+					switch (input) {
+					case 1: break; // 로그인
+					case 2: break; // 회원 가입
+					case 0: System.out.println("\n프로그램 종료"); break;
+					default: System.out.println("메뉴에 작성된 번호만 입력해주세요.");
+					}
+					
+				} else {
+				
+					System.out.println("***** 로그인 메뉴 *****");
+					System.out.println("1. 회원 기능");
+					System.out.println("2. 게시판 기능");
+					System.out.println("0. 로그아웃");
+					System.out.println("99. 프로그램 종료");
+					
+					System.out.print("\n메뉴 선택 : ");
+					input = sc.nextInt();
+					
+					System.out.println();
+					
+					switch (input) {
+					case 1: break;
+					case 2: break;
+					case 0: break;
+					case 99: break;
+					default : 
+					
+						
+				
+					}
+				}	
+			} catch(InputMismatchException e) {
+				System.out.println("\n[입력 형식이 올바르지 않습니다.]");
+				sc.nextLine();
+			}
+			
+		} while (input != 0);
+	}
+	
+	
 }
